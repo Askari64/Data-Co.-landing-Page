c1596cbbcfdee2ac06764f03b5d4d3b0947c7735
Askari <askarir64@gmail.com>
2024-04-26 01:14:18 +0530

    Newsletter Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import AnalyticsSection from "./components/AnalyticsSection";
+import Newsletter from "./components/Newsletter";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Nav />
       <Hero />
       <AnalyticsSection />
+      <Newsletter />
     </>
   );
 }

ee2b561c1cdb8ed115a0c2344c5b7eaa0c308491
Askari <askarir64@gmail.com>
2024-04-26 15:46:40 +0530

    Plans Component Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import AnalyticsSection from "./components/AnalyticsSection";
 import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Hero />
       <AnalyticsSection />
       <Newsletter />
+      <Cards />
     </>
   );
 }
